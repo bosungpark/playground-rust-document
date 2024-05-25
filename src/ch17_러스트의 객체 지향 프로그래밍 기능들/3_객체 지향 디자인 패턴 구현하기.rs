5ff7cae05b4e99b577f89292eef63b763d5b310c
bosungpark <qkrqhtjd0806@naver.com>
2024-05-26 00:02:36 +0900

    pattern

---
+++ src/ch17_러스트의 객체 지향 프로그래밍 기능들/3_객체 지향 디자인 패턴 구현하기.rs
@@ -0,0 +1,101 @@
+fn 상태_패턴_실행_예시() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
+
+fn 객체_지향_디자인_패턴_구현하기() {
+    // 상태 패턴(state pattern): 객체 지향 디자인 패턴
+    // 어떤 값이 내부적으로 가질 수 있는 상태 집합을 정의하는 패턴
+}
+
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+// 핵심은 이 트레이트를 사용해 다형성을 사용한다는 것!
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,57 @@
 fn main() {
-    let mut post = Post::new();
-
-    post.add_text("I ate a salad for lunch today");
-    assert_eq!("", post.content());
-
-    post.request_review();
-    assert_eq!("", post.content());
-
-    post.approve();
-    assert_eq!("I ate a salad for lunch today", post.content());
+    패턴이_사용될_수_있는_모든_곳()
 }
 
-fn 객체_지향_디자인_패턴_구현하기() {
-    // 상태 패턴(state pattern): 객체 지향 디자인 패턴
-    // 어떤 값이 내부적으로 가질 수 있는 상태 집합을 정의하는 패턴
-}
+fn 패턴이_사용될_수_있는_모든_곳() {
+    // 1. match 갈래
 
-pub struct Post {
-    state: Option<Box<dyn State>>,
-    content: String,
-}
+    // match 표현식의 값에 대한 모든 경우의 수를 고려해야 한다는 의미에서 철저해야 한다는 특징
+    // 모든 가능성을 포괄하는 것을 보장하는 방법의 하나는 마지막 갈래에 캐치올 패턴을 사용하는 것
 
-impl Post {
-    pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
-            content: String::new(),
-        }
+    let 숫자 = Some(3);
+
+    match 숫자 {
+        Some(3) => println!("3"),
+        _ => (),
     }
 
-    pub fn add_text(&mut self, text: &str) {
-        self.content.push_str(text);
+    // 2. if let 조건 표현식
+
+    // 컴파일러가 해당 구문이 모든 경우를 빠짐없이 포괄하는지 검사하지 않는다는 특징
+    // else절을 생략하여 처리되지 않는 경우가 생기더라도
+    // 컴파일러는 이에 따라 발생할 수 있는 논리적 버그를 경고해 주지 않는다
+
+    if let Some(3) = 숫자 {
+        println!("3");
     }
 
-    pub fn content(&self) -> &str {
-        self.state.as_ref().unwrap().content(self)
+    // 3. while let 조건 루프
+
+    // while let 조건 루프는 패턴이 계속 매칭되는 동안 while 루프를 실행할 수 있음
+
+    let mut stack = Vec::new();
+
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
     }
 
-    pub fn request_review(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.request_review())
-        }
+    // 4. for 루프
+    let v = vec![3];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
     }
 
-    pub fn approve(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
-        }
-    }
-}
+    // 5. let 구문
 
-trait State {
-    fn request_review(self: Box<Self>) -> Box<dyn State>;
-    fn approve(self: Box<Self>) -> Box<dyn State>;
+    // 알고보면 let 구문을 사용할 때마다 여러분은 패턴을 사용한 것과 같다.
 
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
-        ""
-    }
-}
+    let x = 5;
+    let (x, y, z) = (1, 2, 3);
 
-// 핵심은 이 트레이트를 사용해 다형성을 사용한다는 것!
-struct Draft {}
+    // 6. 함수 매개변수
 
-impl State for Draft {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        Box::new(PendingReview {})
-    }
-
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-}
-
-struct PendingReview {}
-
-impl State for PendingReview {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Published {})
-    }
-}
-
-struct Published {}
-
-impl State for Published {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
-        &post.content
-    }
+    fn foo(x: i32) {}
 }
